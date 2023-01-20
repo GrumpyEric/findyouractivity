6c3a757775bf52748736783ca05bd64d9e5e1bd9
GrumpyEric <55453689+GrumpyEric@users.noreply.github.com>
2023-01-20 17:55:41 +0100

    radius of viewable markers implemented

--- screens/MyMarkersScreen.js
+++ screens/MyMarkersScreen.js
@@ -9,6 +9,7 @@ import { getDistance } from 'geolib';
 import { userPosContext } from '../components/AppContext';
 import Colors from '../constants/Colors';
 import TextButton from '../components/TextButton';
+import Slider from '@react-native-community/slider';
 
 const MyMarkersScreen = ( {navigation} ) => {
 
@@ -23,54 +24,78 @@ const moveToMarker = (inputMarker) => {
 }
   const [showMyMarkers, setShowMyMarkers] = useState(true)
   const myUserID = auth.currentUser.uid
+  const [radiusMarkers, setRadiusMarkers] = useState(5)
+  const [radiusMarkersVisual, setRadiusMarkersVisual] = useState(5)
   // console.log(myUserID);
 
   let myMarkersRef = markersRef.filter(function (arr) {
     return arr.user === myUserID
   })
 
-  console.log(myMarkersRef);
+  // console.log(myMarkersRef);
+
+  useEffect(() => {
+    console.log(radiusMarkers);
+  }, [radiusMarkers])
 
   return (
     <ScrollView >
       <View style={stylesGlobal.screenContainer}>
 
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Text style={{color: showMyMarkers ? Colors.findmyactivityText : Colors.findmyactivityBlue}}>Alle Marker anzeigen</Text>
-          <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={showMyMarkers ? "#f5dd4b" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={() => setShowMyMarkers(!showMyMarkers)}
-            value={showMyMarkers}
-          />
-          <Text style={{color: showMyMarkers ? Colors.findmyactivityBlue : Colors.findmyactivityText}}>Nur meine Marker anzeigen</Text>
+        <View style={{alignItems: 'center', width: '100%'}}>
+          <View style={{flexDirection: 'row', }}>
+            <Text style={{color: showMyMarkers ? Colors.findmyactivityText : Colors.findmyactivityBlue}}>Alle Marker anzeigen</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={showMyMarkers ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setShowMyMarkers(!showMyMarkers)}
+              value={showMyMarkers}
+            />
+            <Text style={{color: showMyMarkers ? Colors.findmyactivityBlue : Colors.findmyactivityText}}>Nur meine Marker anzeigen</Text>
+          </View>
+
+          <View style={{width: '100%'}}>
+            <Text>Radius der anzuzeigenden Marker</Text>
+            <Text>{radiusMarkersVisual} km</Text>
+            <Slider
+              minimumValue={0}
+              maximumValue={21}
+              onSlidingComplete={(value) => value < 21 ? setRadiusMarkers(value) : setRadiusMarkers('alle')}
+              step={1}
+              value={radiusMarkers}
+              onValueChange={(value) => value < 21 ? setRadiusMarkersVisual(value) : setRadiusMarkersVisual('alle')}
+            />
+          </View>
         </View>
         {
           showMyMarkers ?
           myMarkersRef.map((val, index) => 
             {
               let distanceToUserPos = "?"//getDistance(val,props.userPosContext.coords) / 1000
-              console.log(userPosContext._currentValue.coords);
+              // console.log(userPosContext._currentValue.coords);
               if (userPosContext._currentValue.coords != undefined)
               {
                 distanceToUserPos = getDistance(val, userPosContext._currentValue.coords) / 1000
               }
-              return (
-                <View style={{backgroundColor: '#DDDDDD', marginBottom: 10}}>
-                  <TouchableOpacity onPress={() => moveToMarker(val)}>
-                    <Text key={Math.random().toString()}> {"MARKERNAME: " + val.name} </Text>
-                    <Text key={Math.random().toString()}> {"BESCHREIBUNG: " + val.description} </Text>
-                    <Text> Distanz: {distanceToUserPos} km</Text>
-                  </TouchableOpacity>
-                  <TextButton
-                    onPress={() => null}
-                    text={'bearbeiten'}
-                    textColor={Colors.findmyactivityBlue}
-                  />
-                  {/* <Text> Distanz: {distanceToUserPos} km</Text> */}
-                </View>
-              )
+              if (distanceToUserPos < radiusMarkers || radiusMarkers === 'alle') {
+                // console.log(distanceToUserPos);
+                return (
+                  <View style={{backgroundColor: '#DDDDDD', marginBottom: 10}}>
+                    <TouchableOpacity onPress={() => moveToMarker(val)}>
+                      <Text key={Math.random().toString()}> {"MARKERNAME: " + val.name} </Text>
+                      <Text key={Math.random().toString()}> {"BESCHREIBUNG: " + val.description} </Text>
+                      <Text> Distanz: {distanceToUserPos} km</Text>
+                    </TouchableOpacity>
+                    <TextButton
+                      onPress={() => null}
+                      text={'bearbeiten'}
+                      textColor={Colors.findmyactivityBlue}
+                    />
+                    {/* <Text> Distanz: {distanceToUserPos} km</Text> */}
+                  </View>
+                )
+              }
             }
           )
           :
@@ -81,26 +106,28 @@ const moveToMarker = (inputMarker) => {
               {
                 distanceToUserPos = getDistance(val, userPosContext._currentValue.coords) / 1000
               }
-              return (
-                <View style={{backgroundColor: '#DDDDDD', marginBottom: 10}}>
-                  <TouchableOpacity onPress={() => console.log(val)}>
-                    <Text key={Math.random().toString()}> {"MARKERNAME: " + val.name} </Text>
-                    <Text key={Math.random().toString()}> {"BESCHREIBUNG: " + val.description} </Text>
-                    <Text> Distanz: {distanceToUserPos} km</Text>
-                  </TouchableOpacity>
-                  {val.user === myUserID 
-                  ?
-                  <TextButton
-                    onPress={() => null}
-                    text={'bearbeiten'}
-                    textColor={Colors.findmyactivityBlue}
-                  />
-                  :
-                  null
-                  }
-                  {/* <Text> Distanz: {distanceToUserPos} km</Text> */}
-                </View>
-              )
+              if (distanceToUserPos < radiusMarkers || radiusMarkers === 'alle') {
+                return (
+                  <View style={{backgroundColor: '#DDDDDD', marginBottom: 10}}>
+                    <TouchableOpacity onPress={() => console.log(val)}>
+                      <Text key={Math.random().toString()}> {"MARKERNAME: " + val.name} </Text>
+                      <Text key={Math.random().toString()}> {"BESCHREIBUNG: " + val.description} </Text>
+                      <Text> Distanz: {distanceToUserPos} km</Text>
+                    </TouchableOpacity>
+                    {val.user === myUserID 
+                    ?
+                    <TextButton
+                      onPress={() => null}
+                      text={'bearbeiten'}
+                      textColor={Colors.findmyactivityBlue}
+                    />
+                    :
+                    null
+                    }
+                    {/* <Text> Distanz: {distanceToUserPos} km</Text> */}
+                  </View>
+                )
+              }
             }
           )
         }
